7e412918e6a53ac9e6107f24ba05e69b6e44c459
lalekachar <lalekachar@gmail.com>
2023-08-14 13:20:16 -0400

    car and Plain classes added

---
+++ src/Vehicle/Plain.java
@@ -0,0 +1,8 @@
+package Vehicle;
+
+public class Plain {
+
+    public static void main(String[] args) {
+        System.out.println("plain flies");
+    }
+}
---
+++ src/Vehicle/car.java
@@ -0,0 +1,10 @@
+package Vehicle;
+
+public class car {
+    public static void main(String[] args) {
+        for( int i=0; i<10; i++){
+            System.out.println("car Goes");
+        }
+
+    }
+}
