fd536bce1d7d1ba58b1100dc2935e00ef2876def
lalekachar <lalekachar@gmail.com>
2023-08-14 13:30:13 -0400

    variables and getters and setters added to car class, method 1 was added to plain class

--- src/Vehicle/Plain.java
+++ src/Vehicle/Plain.java
@@ -5,4 +5,7 @@ public class Plain {
     public static void main(String[] args) {
         System.out.println("plain flies");
     }
+    public void method1(){
+        System.out.println("plains go fast");
+    }
 }
--- src/Vehicle/car.java
+++ src/Vehicle/car.java
@@ -1,10 +1,41 @@
 package Vehicle;
 
 public class car {
+
+    String  make;
+    String model;
+    int year;
+
     public static void main(String[] args) {
         for( int i=0; i<10; i++){
             System.out.println("car Goes");
         }
 
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+
+
+    }
+
+    public void setMake(String make) {
+        this.make = make;
     }
 }
